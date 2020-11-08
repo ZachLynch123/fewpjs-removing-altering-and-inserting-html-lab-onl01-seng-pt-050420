c93ae4cb4c5ce727c0b8273bcb5bb23cfbe3a2a6
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-11-08 18:45:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Write your code here!
 document.querySelector('main').remove();
 
-let newHeader  = document.createElement("h1#victory")
+let newHeader  = document.createElement("h1#victory");
+document.body.appendChild(newHeader)
 
 newHeader= document.querySelector("h1#victory");
 newHeader.innerHTML = "Zach is the champion";
